d408dad78135e2d09b3aa9d350758890c82600db
Katrina Uychaco <kuychaco@gmail.com>
2015-09-10 15:26:08 -0700

    Add numbers for exercises.

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-// Write a function, `AnimalTestUser`, that has one string parameter, `username`. It returns an object with a username property.
+// 1. Write a function, `AnimalTestUser`, that has one string parameter, `username`. It returns an object with a username property.
 
 // ```javascript
 // var testSheep = AnimalTestUser('CottonBall');
@@ -7,7 +7,7 @@
 
 
 
-// In your `AnimalTestUser` function, create a check that sees how many arguments are passed. If there is more than one argument, create a property, `otherArgs` that is an array of the remaining arguments. Note: the `arguments` keyword is not a true array. Remember, it is an array-like object.
+// 2. In your `AnimalTestUser` function, create a check that sees how many arguments are passed. If there is more than one argument, create a property, `otherArgs` that is an array of the remaining arguments. Note: the `arguments` keyword is not a true array. Remember, it is an array-like object.
 
 // ```javascript
 // var testSheep = AnimalTestUser('CottonBall', {'loves dancing': true}, [1,2,3] );
@@ -16,7 +16,7 @@
 
 
 
-// Write a constructor function, `AnimalCreator` that returns a single animal object. The constructor function has 4 parameters: `username`, `species`, `tagline,` and `noises`. The animal object should have at least 5 properties: `username`, `species`, `noises`, `tagline`, and `friends`. The values should all be strings except `noises` and `friends`, which are arrays. 
+// 3. Write a constructor function, `AnimalCreator` that returns a single animal object. The constructor function has 4 parameters: `username`, `species`, `tagline,` and `noises`. The animal object should have at least 5 properties: `username`, `species`, `noises`, `tagline`, and `friends`. The values should all be strings except `noises` and `friends`, which are arrays. 
 
 // ```javascript
 // var sheep = AnimalCreator('Cloud', 'sheep', 'You can count on me!', ['baahhh', 'arrgg', 'chewchewchew']);
@@ -31,7 +31,7 @@
 
 
 
-// Write a function, `addFriend` that takes an animal object (like the one returned from the `AnimalCreator` function) and adds another animal object as a friend. 
+// 4. Write a function, `addFriend` that takes an animal object (like the one returned from the `AnimalCreator` function) and adds another animal object as a friend. 
 
 // ```javascript
 //   addFriend(sheep, cow);
@@ -54,7 +54,7 @@
 
 
 
-// Change your `addFriend` function to only add the username of the friend, not the whole object.
+// 5. Change your `addFriend` function to only add the username of the friend, not the whole object.
 
 // ```javascript
 //   addFriend(sheep, cow);
@@ -77,7 +77,7 @@
 
 
 
-// Create a `myFarm` collection of at least 3 animal objects. Give them some friends using `addFriend`, too!
+// 6. Create a `myFarm` collection of at least 3 animal objects. Give them some friends using `addFriend`, too!
 
 // ```javascript 
 // console.log(myFarm) //[{username: 'Cloud'...},{username: 'Zeny'...},{username: 'CottonBall'...}]
@@ -85,7 +85,7 @@
 
 
 
-// Create a function, `addMatchesArray`, that takes a farm (array of animal objects) and adds a new property to each animal object called `matches` that is an empty array. Hint: you will need a loop. 
+// 7. Create a function, `addMatchesArray`, that takes a farm (array of animal objects) and adds a new property to each animal object called `matches` that is an empty array. Hint: you will need a loop. 
 
 // ```javascript 
 // addMatchesArray(myFarm); 
@@ -100,7 +100,7 @@
 
 
 
-// Create a function, `giveMatches`, that takes a farm collection (aka an array of animal objects) that already has a matches property. It selects a name from the `friends` array and adds it to the `matches` array. You can choose how the selection is made (random, the first element, etc). Make sure all your animal objects have friends.
+// 8. Create a function, `giveMatches`, that takes a farm collection (aka an array of animal objects) that already has a matches property. It selects a name from the `friends` array and adds it to the `matches` array. You can choose how the selection is made (random, the first element, etc). Make sure all your animal objects have friends.
 
 // ```javascript 
 // giveMatches(myFarm); 
