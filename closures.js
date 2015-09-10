b316db0c3521da5f101e95f5acf31b5db5650e0c
Katrina Uychaco <kuychaco@gmail.com>
2015-09-10 15:20:37 -0700

    Add numbers for exercises.

--- closures.js
+++ closures.js
@@ -1,6 +1,6 @@
 
 
-// Write a function, `nonsense` that takes an input `string`. This function contains another function, `blab` which alerts `string` and is immediately called inside the function `nonsense`. `blab` should look like this inside of the `nonsense` function:
+// 1. Write a function, `nonsense` that takes an input `string`. This function contains another function, `blab` which alerts `string` and is immediately called inside the function `nonsense`. `blab` should look like this inside of the `nonsense` function:
 
 // 	```javascript
 // 	 var blab = function(){
@@ -8,14 +8,14 @@
 // 	 };
 // 	 ```
 
-// In your function, `nonsense`, change the immediate call to a setTimeout so that the call to `blab` comes after 2 seconds. The `blab` function itself should stay the same as before.
+// 2. In your function, `nonsense`, change the immediate call to a setTimeout so that the call to `blab` comes after 2 seconds. The `blab` function itself should stay the same as before.
 
-// Now, instead of calling `blab` inside of `nonsense`, return `blab` (without invoking it). Call `nonsense` with some string and store the returned value (the `blab` function) in a variable called `blabLater`. Call `nonsense` again with a different string and store the returned value in a variable called `blabAgainLater`.
+// 3. Now, instead of calling `blab` inside of `nonsense`, return `blab` (without invoking it). Call `nonsense` with some string and store the returned value (the `blab` function) in a variable called `blabLater`. Call `nonsense` again with a different string and store the returned value in a variable called `blabAgainLater`.
 
-// Inspect `blabLater` and `blabAgainLater` in your console. Call them (they are functions!) and see what happens!
+// 4. Inspect `blabLater` and `blabAgainLater` in your console. Call them (they are functions!) and see what happens!
 
 
-// Write a function with a closure. The first function should only take one argument, someone's first name, and the inner function should take one more argument, someone's last name. The inner function should console.log both the first name and the last name.
+// 5. Write a function with a closure. The first function should only take one argument, someone's first name, and the inner function should take one more argument, someone's last name. The inner function should console.log both the first name and the last name.
 // 	```javascript
 // 	var lastNameTrier = function(firstName){
 // 	   //does stuff
@@ -36,7 +36,7 @@
 // 	```       
        
 
-// Create a `storyWriter` function that returns an object with two methods. One method, `addWords` adds a word to your story and returns the story while the other one, `erase`, resets the story back to an empty string. Here is an implementation:
+// 6. Create a `storyWriter` function that returns an object with two methods. One method, `addWords` adds a word to your story and returns the story while the other one, `erase`, resets the story back to an empty string. Here is an implementation:
 // 	```javascript
 // 	var farmLoveStory = storyWriter();
 // 	farmLoveStory.addWords('There was once a lonely cow.'); // 'There was once a lonely cow.'
@@ -49,7 +49,7 @@
 	
 // 	```  
 
-// Using the module pattern, design a toaster. Use your creativity here and think about what you want your users to be able to access on the outside of your toaster vs what you don't want them to be able to touch.
+// 7. Using the module pattern, design a toaster. Use your creativity here and think about what you want your users to be able to access on the outside of your toaster vs what you don't want them to be able to touch.
 		
 // 	```javascript
 // 	var Toaster = function(){
